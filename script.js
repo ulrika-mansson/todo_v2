729fddb08625a74bad08500c4c7015da5e00da75
ulrika-mansson <32763965+ulrika-mansson@users.noreply.github.com>
2017-11-20 13:52:25 +0100

    ändringar söndag
    utloggning, kontroll inloggad eller inte

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 $(document).ready(function(){
     console.log("fungerar?");
 
+    
+
     //sätt variabler
     var ourUser = "test"
     var ourPassword = "password"
+    sessionStorage.isLoggedIn = false;
+    console.log(sessionStorage.isLoggedIn);
 
     //dölj forgotLogin och correctLogin
     $(".forgotLogin").hide();
     $(".correctLogin").hide();
+    $(".logOut").hide();
 
     //funktion för att logga in med knapp
-    $(".submitButton").click(function(){
+    $(".logInButton").click(function(){
 
         if (ourUser == $(".userName").val() && ourPassword == $(".passwordUser").val()) {
             
             console.log("user mail is ok");
             // Dölj helloForm, visa correctLogin
-                
+            // dölj logInForm, visa logOut    
                             
             $(".helloForm").hide();
+            $(".forgotLogin").hide();
             $(".correctLogin").show();
+            sessionStorage.isLoggedIn = true;
+            $(".logInForm").hide();
+            $(".logOut").show();
+
+            console.log(sessionStorage.isLoggedIn);
             
         } else {
             console.log("fel ifyllt")
@@ -32,6 +43,15 @@ $(document).ready(function(){
             }
             
         });
+
+        //funktion för att logga ut med knapp
+            $(".logOutButton").click(function(){
+
+                //dölj logut, visa log in
+                $(".logOut").hide();
+                $(".logInForm").show();
+                sessionStorage.isLoggedIn = false;
+            });
 
         //visa datum - jag är så nöjd att jag vill dricka sprit
             var date = new Date;
