8bb53b2ecf4ae4bd33996bb781a1b23d0800981f
ulrika-mansson <32763965+ulrika-mansson@users.noreply.github.com>
2018-01-03 10:47:08 +0100

    uppdaterad med länk

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # todo_v2
+ inloggning: test
+ password: password
+
+ länk:
+ http://ulrikamansson.wieg17.se/projekt/todo/
---
+++ script - kopia.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+    console.log("fungerar?");
+
+    
+    //sätt globala variabler
+    var ourUser = "test"
+    var ourPassword = "password"
+
+    if (localStorage.doList == null) {
+        var stuffToDo = [
+            "Klipp gräset", 
+            "Betala räkningar",
+            "Köp mjölk", 
+            "Spika upp tavlor"
+            ];
+
+            var json_str = JSON.stringify (stuffToDo);
+            localStorage.doList = json_str;
+    };
+
+    console.log(sessionStorage.ourUser);
+
+    if (sessionStorage.ourUser != null) {
+            showCorrectLogIn();
+    } else {
+        showHelloForm();
+    }
+ 
+
+   //funktion för att logga in med knapp
+    $(".logInButton").click(function(){
+
+        if (ourUser == $(".userName").val() && ourPassword == $(".passwordUser").val()) 
+        
+        {
+            showCorrectLogIn();
+            
+   
+        } else {
+                       
+            // Dölj helloForm, visa forgotLogin
+            showForgotLogin();
+            }
+            
+        });
+
+
+        
+        //funktion för att logga ut med knapp
+            $(".logOutButton").click(function(){
+
+                //dölj logut, visa log in och welcomeBack
+                showWelcomeBack();
+
+                sessionStorage.removeItem("ourUser");
+                console.log(sessionStorage.ourUser);
+            });
+
+        
+            //visa datum - jag är så nöjd att jag vill dricka sprit
+            var date = new Date;
+            console.log(date);
+            /* $(".todaysDate").append(date); */
+            $(".todaysDate").append(date.getFullYear(), "-", date.getMonth(), "-", date.getDate());
+
+            //funktion för att visa startsida
+        function showHelloForm() {
+            $(".helloForm").show();
+            $(".forgotLogin").hide();
+            $(".correctLogin").hide();
+            $(".logOut").hide();
+            $(".welcomeBack").hide();
+            };
+            
+            //funktion för att visa rätt inloggning
+            function showCorrectLogIn() {
+                $(".helloForm").hide();
+                $(".forgotLogin").hide();
+                $(".correctLogin").show();
+                $(".logInForm").hide();
+                $(".logOut").show();
+                $(".welcomeBack").hide();
+                sessionStorage.ourUser = $(".userName").val();
+                };
+
+        //funktion för att visa glömt inloggning
+        function showForgotLogin() {
+            $(".helloForm").hide();
+            $(".forgotLogin").show();
+            $(".correctLogin").hide();
+            $(".welcomeBack").hide();
+            $(".logOut").hide();
+        };
+
+
+        //funktion för att visa välkommen tillbaka efter inloggning
+        function showWelcomeBack() {
+            $(".logOut").hide();
+            $(".correctLogin").hide();
+            $(".logInForm").show();
+            $(".welcomeBack").show();
+            sessionStorage.isLoggedIn = false;
+        };
+})
+
+        
+        /*lägg arrayen i localStorage yada yada bla bla*/
+        var stuffToDo = JSON.parse(localStorage.doList);
+
+        /*var myJSON = JSON.stringify(stuffToDo);
+        var todoelement = document.getElementById("toDoList"); 
+        stuffToDo.forEach(function(element) {
+            todoelement.appendChild */
+
+        printArray();
+
+        function printArray() {
+            var printList = "";
+            for(var i = 0; i < stuffToDo.length; i++) {
+                printList += "<li>" + stuffToDo[i] + " <a href='#' onclick='deleteItem(" + i + ")' >Radera</a>" + "</li>";
+            }
+            console.log(stuffToDo);
+            //ändra Radera till en fontawesome ikon
+            
+        document.getElementById("toDoList").innerHTML = printList;
+    };
+
+    /* function addItem() { */
+        //lägg till ett inputfält i html, med en knapp
+        $(".addItemButton").click(function(){ 
+            if($(".addItem").val() != '') 
+            {
+                stuffToDo.push($(".addItem").val());
+                $(".addItem").val('');
+                console.log($(".addItem").val());
+                printArray();
+                console.log(stuffToDo);
+            }
+        });
+
+    
+    function deleteItem(i) {
+        stuffToDo.splice(i,1);
+        printArray();
+        console.log(stuffToDo);
+    };
+                    
+
+        
